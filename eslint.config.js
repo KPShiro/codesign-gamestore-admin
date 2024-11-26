f7d7d187550b2ad085aa7a703edd4eae3911786b
Kacper Popczyński <popkacper@gmail.com>
2024-11-26 12:14:56 +0100

    Added Storybook to the project

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default tseslint.config(
             },
         },
         extends: [
+            'plugin:storybook/recommended',
             js.configs.recommended,
             ...tseslint.configs.recommendedTypeChecked,
             ...tseslint.configs.strictTypeChecked,
