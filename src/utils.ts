bd1552b67d48c61ed10c2a9fc2aac1b2e30a116f
Kacper Popczyński <popkacper@gmail.com>
2025-01-10 17:52:34 +0100

    Added converBytes util

--- src/utils.ts
+++ src/utils.ts
@@ -24,6 +24,24 @@ export function getBrowserLocale(): string {
     return navigator.language ?? 'en-US';
 }
 
+type Unit = 'B' | 'KB' | 'MB' | 'GB' | 'TB';
+
+export function convertBytes(bytes: number, targetUnit: Unit) {
+    const units: { [key in Unit]: number } = {
+        B: 1,
+        KB: 1024,
+        MB: 1024 ** 2,
+        GB: 1024 ** 3,
+        TB: 1024 ** 4,
+    };
+
+    if (!units[targetUnit]) {
+        throw new Error(`Unsupported unit: ${targetUnit}`);
+    }
+
+    return bytes / units[targetUnit];
+}
+
 export function formatNumber(value: number): string {
     const browserLocale = getBrowserLocale();
     const formatter = new Intl.NumberFormat(browserLocale, {
