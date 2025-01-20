0dc5f3e828f791b26f4feb093241e4db707eaa91
Kacper Popczyński <popkacper@gmail.com>
2025-01-20 20:08:51 +0100

    Updated Dialog component

--- src/features/dialog/components/dialog.tsx
+++ src/features/dialog/components/dialog.tsx
@@ -2,7 +2,6 @@
 
 import { cn } from '@/utils';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
-import { X } from 'lucide-react';
 import React from 'react';
 
 const Dialog = ({ ...props }: React.ComponentProps<typeof DialogPrimitive.Root>) => {
@@ -45,11 +44,8 @@ const DialogContent = React.forwardRef<
             )}
             {...props}
         >
-            <div className="relative grid w-screen max-w-lg gap-6 rounded-md border bg-background p-6 shadow-lg">
+            <div className="relative grid w-screen max-w-lg rounded-md border bg-background shadow-lg">
                 {children}
-                <DialogPrimitive.Close className="focus:ring-ring absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-                    <X className="h-4 w-4" />
-                </DialogPrimitive.Close>
             </div>
         </DialogPrimitive.Content>
     </DialogPortal>
@@ -57,20 +53,32 @@ const DialogContent = React.forwardRef<
 
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
-const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-    <div className={cn('flex max-w-prose flex-col gap-y-3', className)} {...props} />
-);
-
-DialogHeader.displayName = 'DialogHeader';
-
 const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
     <div
-        className={cn('flex flex-col-reverse gap-2 sm:flex-row sm:justify-end', className)}
+        className={cn(
+            'flex flex-col-reverse gap-2 border-t p-6 sm:flex-row sm:justify-end',
+            className
+        )}
         {...props}
     />
 );
 
 DialogFooter.displayName = 'DialogFooter';
+
+const DialogBody = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+    <div
+        className={cn('flex max-h-[50vh] flex-col gap-3 overflow-y-auto border-t p-6', className)}
+        {...props}
+    />
+);
+
+DialogBody.displayName = 'DialogBody';
+
+const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+    <div className={cn('flex max-w-prose flex-col gap-y-1.5 p-6', className)} {...props} />
+);
+
+DialogHeader.displayName = 'DialogHeader';
 
 const DialogTitle = React.forwardRef<
     React.ElementRef<typeof DialogPrimitive.Title>,
@@ -91,7 +99,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
     <DialogPrimitive.Description
         ref={ref}
-        className={cn('text-sm text-muted-foreground', className)}
+        className={cn('max-w-prose text-sm text-foreground/65', className)}
         {...props}
     />
 ));
@@ -102,6 +110,7 @@ Dialog.Close = DialogClose;
 Dialog.Content = DialogContent;
 Dialog.Description = DialogDescription;
 Dialog.Footer = DialogFooter;
+Dialog.Body = DialogBody;
 Dialog.Header = DialogHeader;
 Dialog.Title = DialogTitle;
 Dialog.Trigger = DialogTrigger;
