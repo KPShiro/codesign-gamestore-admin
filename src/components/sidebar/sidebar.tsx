b5558705a3f83e4267f27adc3091a37b5c53f8fb
Kacper Popczyński <popkacper@gmail.com>
2025-02-20 10:29:10 +0100

    feat: Updated sidebar buttons

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,13 +3,11 @@ import SidebarButton from './sidebar-button';
 
 const Sidebar = () => {
     return (
-        <aside className="flex flex-col gap-4 bg-foreground/5 px-4 py-6">
-            <div className="flex flex-col gap-2">
-                <SidebarButton to="fake-casino" icon={PlayIcon} text="Fake Casino" />
-                <SidebarButton to="catalog" icon={GamepadIcon} text="Games Catalog" />
-                <SidebarButton to="management/jackpots" icon={PiggyBankIcon} text="Jackpots" />
-                <SidebarButton to="management/geoblock" icon={MapPinOffIcon} text="Geoblock" />
-            </div>
+        <aside className="bg-foreground/5">
+            <SidebarButton to="fake-casino" icon={PlayIcon} text="Fake Casino" />
+            <SidebarButton to="catalog" icon={GamepadIcon} text="Games Catalog" />
+            <SidebarButton to="management/jackpots" icon={PiggyBankIcon} text="Jackpots" />
+            <SidebarButton to="management/geoblock" icon={MapPinOffIcon} text="Geoblock" />
         </aside>
     );
 };
